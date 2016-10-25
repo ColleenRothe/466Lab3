98cb0a85f466595f286d6a9325a9344cecd90252
ColleenRothe <colleenrothe516@gmail.com>
2016-10-24 21:11:51 -0600

    new network topology

--- simulation_3.py
+++ simulation_3.py
@@ -3,6 +3,7 @@ Created on Oct 12, 2016
 
 @author: mwitt_000
 '''
+import queue
 import network
 import link
 import threading
@@ -22,7 +23,9 @@ if __name__ == '__main__':
     server = network.Host(2)
     object_L.append(server)
 
-    #started to create the new configuration.....
+    other = network.Host(3)
+    object_L.append(other)
+
     router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
 
@@ -43,14 +46,31 @@ if __name__ == '__main__':
     object_L.append(link_layer)
 
     # add all the links
+    #client=1, server=2, other=3
+
+    #top path
     link_layer.add_link(link.Link(client, 0, router_a, 0, 50))  # mtu=50
-    link_layer.add_link(link.Link(router_a, 0, server, 0, 30)) #changed this MTU!
+    link_layer.add_link(link.Link(router_a, 0, router_b, 0, 50))  # mtu=50
+    link_layer.add_link(link.Link(router_b, 0, router_d, 0, 50))  # mtu=50
+    link_layer.add_link(link.Link(router_d, 0, other, 0, 50))  # mtu=50
+
+    #bottom path
+    link_layer.add_link(link.Link(server, 0, router_a, 0, 50))  # mtu=50
+    link_layer.add_link(link.Link(router_a, 0, router_c, 0, 50))  # mtu=50
+    link_layer.add_link(link.Link(router_c, 0, router_d, 0, 50))  # mtu=50
 
     # start all the objects
     thread_L = []
     thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
     thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
+    thread_L.append(threading.Thread(name=other.__str__(), target=other.run))
     thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
+
+
+
 
     thread_L.append(threading.Thread(name="Network", target=link_layer.run))
 
